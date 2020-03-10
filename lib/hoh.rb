3ecdd55a4943e5f29874a3c1f0c78188b429fe8f
Paul Ralyea <047par@gmail.com>
2020-03-10 14:47:04 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,6 +31,39 @@
 # }
 
 def naming_system
+  hash = {label: "Kingdom",
+   sub_category: {
+     label: "Phylum",
+     sub_category: {
+      label: "Class",
+      sub_category: {
+          label: "Order",
+          sub_category: {
+              label: "Family",
+              sub_category: {
+                  label: "Genus",
+                  sub_category: {
+                    label: "Species",
+                    sub_category: nil
+ }
+              }
+          }
+      }
+     }
+   }
+  }
+                    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   # Remember:
   #  Kingdom
   #  Phylum
